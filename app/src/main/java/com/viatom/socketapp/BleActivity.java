c2579f98b7ad42a97c40dbcfda3450fd9323271d
viatom4ewater <mowenjian@viatomtech.com>
2020-06-24 21:11:56 +0800

    重连socket设置

--- app/src/main/java/com/viatom/socketapp/BleActivity.java
+++ app/src/main/java/com/viatom/socketapp/BleActivity.java
@@ -383,6 +383,7 @@ public class BleActivity extends AppCompatActivity {
 
     private void reSocketConnect(){
         Log.i(TAG,"reSocketConnect");
+        //
         handler.sendEmptyMessageDelayed(201,1000);
     }
 
