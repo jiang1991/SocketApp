e2da2c1ddeb2fcbefe2c882e2610a22c4f378bf3
viatom4ewater <mowenjian@viatomtech.com>
2020-09-10 09:35:53 +0800

    区分国内和国外版本

--- app/src/main/java/com/viatom/socketapp/JavaSocketUtils.java
+++ app/src/main/java/com/viatom/socketapp/JavaSocketUtils.java
@@ -10,6 +10,22 @@ public class JavaSocketUtils {
 
      *****/
 
+    private static String SOCKET_URL_FOREIGN = "http://socket.viatomtech.com.cn/";
+    private static String SOCKET_URL_DOMESTIC ="http://sockettest.viatomtech.com.cn/";
+
     public static String SOCKET_URL = "http://sockettest.viatomtech.com.cn/";
+
+    /***
+    是否是国外
+    *****/
+    public static boolean isForeign=false;
+
+    public static void initParams(){
+        if(isForeign){
+            SOCKET_URL = SOCKET_URL_FOREIGN;
+        }else{
+            SOCKET_URL =  SOCKET_URL_DOMESTIC ;
+        }
+    }
 
 }
--- app/src/main/java/com/viatom/socketapp/MainActivity.java
+++ app/src/main/java/com/viatom/socketapp/MainActivity.java
@@ -98,6 +98,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
+        JavaSocketUtils.initParams();
         //SocketUtilsKt.initGlobalParams();
         initView();
         //ButterKnife.bind(this);
